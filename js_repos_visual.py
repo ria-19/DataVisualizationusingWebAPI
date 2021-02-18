58c7c9a6be66b1d99847f54305e911bc19194a34
Riya <riasangwan1999@gmail.com>
2021-02-18 13:11:45 +0530

    First commit

---
+++ js_repos_visual.py
@@ -0,0 +1,70 @@
+import requests
+
+from plotly.graph_objs import Bar 
+from plotly import offline
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Store an API response in a variable. ---JSON format recieved converted into python dicts
+response_dict = r.json()
+
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+
+#Process Results
+repo_links, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href = '{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+    stars.append(repo_dict['stargazers_count'])
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+
+# Make Visualization.
+data = [{
+    'type': 'bar',
+    'x': repo_links,
+    'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {
+            'width': 1.5,
+            'color': 'rgb(25, 25, 25)',
+        },
+        'opacity': 0.6,
+    }
+}]
+
+my_layout = {
+    'title': 'Most-Starred Javascript Projects on GitHub',
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {
+            'size': 24,
+        },
+        'tickfont': {
+            'size': 14,
+        },
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {
+            'size': 24,
+        },
+        'tickfont': {
+            'size': 14,
+        },
+    },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='javascript_repos.html')
